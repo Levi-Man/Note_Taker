dc4030ff4597535a69f9d026a5bcf6535f5d8af8
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2023-12-14 09:39:39 -0500

    initial commit

---
+++ routes/notes.js
@@ -0,0 +1,40 @@
+const note = require('express').Router();
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the notes
+note.get('/', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+)
+// .then((data) => {
+
+// });
+
+// POST Route for submitting note
+note.post('/', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
+});
+
+module.exports = note;
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const noteRoute = require('./routes/notes')
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
+app.use('/api/notes', noteRoute);
+app.use(express.static('public'));
+
+const notes = [];
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  notes.push(newNote);
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const idToDelete = req.params.id;
+  // Implement logic to delete the note with the specified ID
+  // ...
+  res.json({ success: true });
+});
+
+
+//GET route for homepage
+app.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET route for notes page
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET route for 404
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
